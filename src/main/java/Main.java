dc5ac0f2acb8d3881539ac65bf1e3810609a9eaf
brunovsilv <169068441+brunovsilv@users.noreply.github.com>
2024-05-13 15:21:20 +0000

    13/05

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("++++++++++++++++++");
-    System.out.println("+Artista: Massaru+ ");
-  System.out.println("++++++++++++++++++");
+    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("-Artista: Massaru");
+    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("-Álbum: Majin Mixtape-");
+    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("-Ano de lançamento: Final de 2023-");
+    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("-O álbum Msjin Mixtape foi inspirado no passado de massaru.Já em uma mixtape, as faixas podem ser compiladas de uma forma mais aleatória ou sem nenhuma ligação entre as mesmas.O álbum Majin Mixtape contém 12 faixas-.");
+    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.out.println("-Faixa 1: 1. Massaru, Stuani - Intro-");
+    System.out.println("-Faixa 2: 2. Massaru, Rare Pacheco - Prince-");
+    System.out.println("-Faixa 3: 3. Massaru, - Só/Sim ou Não-");
+    System.out.println("-Faixa 4: 4. Massaru, EF - Aham-");
+    System.out.println("-Faixa 5: 5. Massaru, O adotado - Interludio-");
+    System.out.println("-Faixa 6: 6. Massaru, Clouds - Jogo de Azar-");
+    System.out.println("-Faixa 7: 7. Massaru, - Shake Freestyle [prod. Seithén]-");
+    System.out.println("-Faixa 8: 8. Massaru, - Amaterasu [prod. EF]");
+    System.out.println("-Faixa 9: 9. Massaru, Clouds - Sesc-");
+    System.out.println("-Faixa 10:10.Massaru - Nata [Prod. EF] [Official Video]");
+    System.out.println("-Faixa 11: 11.Massaru, EF - Só-");
+    System.out.println("-Faixa 12: 12. Massaru, EF, Lee San - Sim ou Não-");
   }
 }
